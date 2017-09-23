0f2009d2f2d8d4e8f6c7f121936f594632caf427
zhangdavids <1269969739@qq.com>
2017-09-24 00:53:14 +0800

    add login and register

--- wetalk/assets/js/App.vue
+++ wetalk/assets/js/App.vue
@@ -1,36 +1,62 @@
 <template>
+
 <div class="container-fluid app">
   <nav class="navbar navbar-default app__navbar">
+
     <div class="container-fluid ">
       <div class="navbar-header">
         <router-link to="/" class="navbar-brand">Wetalk</router-link>
       </div>
-
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
           <li>
-            <router-link to="/about">关于</router-link>
+            <router-link to="/about">About</router-link>
           </li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#" class="navbar-btn">注册</a></li>
-          <li><a href="#" class="navbar-btn">登陆</a></li>
+          <li v-if="!$store.state.me.username">
+            <a @click="$store.commit('showLoginForm')">登陆</a>
+          </li>
+          <li class="dropdown" v-if="$store.state.me.username">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <avatar :alt="$store.state.me.username" :size="28"></avatar>
+              <span class="caret"></span>
+            </a>
+
+              <ul class="dropdown-menu">
+                <li><a href="#">my homepage</a> </li>
+                <li><a href="#">personal settings</a> </li>
+                  <li role="separator" class="divider"></li>
+                <li><router-link to="/">login out</router-link></li>
+              </ul>
+          </li>
         </ul>
       </div>
     </div>
   </nav>
+
   <router-view></router-view>
+
+  <div class="app__footer">
+    <div class="container app__container">
+      Copyright @ 2017 By John Cheung
+    </div>
+  </div>
+
+  <login-form v-if="$store.state.loginForm"></login-form>
 </div>
 </template>
 
 <script>
-	export default {
-		data() {
-      return {
-        msg: 'Hello Vue!'
-      }
-		}
-	}
+  import Avatar from './components/Avatar.vue'
+  import LoginForm from './components/LoginForm.vue'
+
+  export default {
+    components: {
+      Avatar,
+      LoginForm
+    }
+  }
 </script>
 
 <style>
@@ -51,5 +77,15 @@ body {
 .app .app__navbar {
   background: #fff;
   border: 0;
+}
+.app__container {
+  width: 940px;
+  margin: 0 auto;
+}
+.app__footer {
+  border-top: 1px solid #eee;
+  padding: 16px 0 24px;
+  color: #999;
+  font-size: 14px;
 }
 </style>
---
+++ wetalk/assets/js/components/PostItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- 使用了 Bootstrap 中的 media 组件 -->
+  <div class="media post">
+    <div class="media-left">
+      <a href="#">
+        <!-- 注意这里使用了方形的头型，头像尺寸为 45px -->
+        <avatar :alt="post.author.name" :size="45" class="v-avatar--squared"></avatar>
+      </a>
+    </div>
+    <div class="media-body">
+      <h4 class="media-heading" v-text="post.title"></h4>
+      <div class="post__meta">1 minutes ago</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Avatar from './Avatar.vue'
+
+  export default {
+       // 接收一个 post 作为参数
+    props: ['post'],
+    data() {
+      return {
+      }
+    },
+    components: {
+      Avatar
+    }
+  }
+</script>
+
+<style>
+  .post {
+    margin-bottom: 15px;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 3px;
+  }
+  .post__meta {
+    color: #999;
+  }
+</style>
+
