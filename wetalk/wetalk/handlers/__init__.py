9677d0e2b524938a1a5ef872ba8e1d17697bdfe2
zhangdavids <1269969739@qq.com>
2017-10-01 10:52:13 +0800

    Add react

--- wetalk/wetalk/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    wetalk.handlers
-    ~~~~~~~~~~~~~~~
-
-    :copyright: (c) 2017 by protream.
-"""
-from .front import front
--- wetalk/wetalk/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-"""
-    wetalk.models
-    ~~~~~~~~~~~~~
-
-    :copyright: (c) 2017 by protream.
-"""
-from .base import db
