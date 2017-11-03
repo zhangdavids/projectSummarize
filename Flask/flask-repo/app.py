485118528b1a2f1b56038169afc404f89b4c2d90
zhangdavids <1269969739@qq.com>
2017-11-03 21:19:15 +0800

    add flask init

---
+++ Flask/flask-repo/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_object(os.getenv('APP_SETTINGS'))
+
+
+
+@app.route('/')
+def hello():
+    return "Hello world"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run()
