b853230775755e8e99bb2a374bdb9248dd0dafcd
john cheung <1269969739@qq.com>
2018-05-01 20:10:00 +0800

    Create merge_sort.py

---
+++ Algorithm/merge_sort.py
@@ -0,0 +1,17 @@
+# 归并排序 递归
+def mergesort(seq):
+    mid = len(seq)//2
+    lft, rgt = seq[:mid], seq[mid:]
+    if len(lft) > 1:
+        lft = mergesort(lft)
+    if len(rgt) > 1:
+        rgt = mergesort(rgt)
+    
+    res = []
+    while lft and rgt:
+        if lft[-1] >= rgt[-1]:
+            res.append(lft.pop())
+        else:
+            res.append(rgt.pop())
+    res.reverse()
+    return (lft or rgt) + res
