dd07b27bf9f4faba76580bc580c8958779b67da9
john cheung <1269969739@qq.com>
2018-05-04 00:32:56 +0800

    python3 实现

--- Daily/180506.py
+++ Daily/180506.py
@@ -5,6 +5,12 @@ David Gale and Lloyd Shapley 1962
 ...
 1.每个未婚男士都必须向他喜欢的但是尚未求过婚的女士求婚
 2.每位女士都会（暂时）与自己喜欢的追求者订婚，并拒绝其他求婚
+
+有n位男士n位女士，每位男士对所有女士按照他喜欢的程度进行排名，
+同时，每位女士也对所有男士有一个喜爱程度排名，无并列。 
+比如我们现在有4位男士：m1，m2，m3，m4，和四位女士w1，w2，w3，w4（m的意思就是man男士，w的意思就是woman女士）。
+第一位男士对所有女士的喜爱排名为：’w3’,’w2’,’w1’,’w4’，即他最喜欢的是第三位女士w3，接着是第二位，第一位，
+最后是第四位女士w4。 
 """
 
 import sys
@@ -14,11 +20,11 @@ class Person(object):
     def __init__(self, name, priorities):
         self.name = name
         self.priorities = priorities
-        self.parter = parter
+        self.partner = None
         
     def __str__(self):
         return "Name is " + self.name + "\n" + \
-               "Parter is currently " + str(self.parter) + "\n" + \
+               "partner is currently " + str(self.partner) + "\n" + \
                "Priority list is " + str(self.priorities)
                
                
@@ -45,30 +51,29 @@ class Woman(Person):
             self.ranking[priorities[rank]] = rank
             
     def evaluate_proposal(self, suiter):
-        return self.parter == None or self.ranking[suitor] < self.ranking[self.parter]
+        return self.partner == None or self.ranking[suiter] < self.ranking[self.partner]
         
         
         
 def parse_file(filename):
     people = []
-    f = file(filename)
-    for line in f:
-        pieces = line.split(":")
-        name = pieces[0].strip()
-        if name:
-            priorities = pieces[1].strip().split(",")
-            for i in range(len(priorities)):
-                priorities[i] = priorities[i].strip()
+    with open(filename, 'r') as f:
+        for line in f:
+            pieces = line.split(":")
+            name = pieces[0].strip()
+            if name:
+                priorities = pieces[1].strip().split(",")
+                for i in range(len(priorities)):
+                    priorities[i] = priorities[i].strip()
+                    
+                people.append((name, priorities))
                 
-            people.append((name, priorities))
-                
-    f.close()
     return people
     
     
 def print_pairings(men):
     for man in men.values():
-    print(man.name, ' is paired with ', str(man.parter))
+        print(man.name, ' is paired with ', str(man.partner))
     
     
 
@@ -77,11 +82,11 @@ if __name__ == "__main__":
     verbose = len(sys.argv)>3
     
     # initialize dictionary of men
-    menlist = parseFile(sys.argv[1])
+    menlist = parse_file(sys.argv[1])
     men = dict()
     for person in menlist:
         men[person[0]] = Man(person[0],person[1])
-    unwedMen = men.keys()
+    unwedMen = list(men.keys())
     
     # initialize dictionary of women
     womenlist = parse_file(sys.argv[2])
@@ -126,8 +131,4 @@ if __name__ == "__main__":
     # we should be done
     print("Final Pairings are as follows:")
     print_pairings(men)
-          
-    
-
-    
-  
+           
