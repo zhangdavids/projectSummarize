816c8311356d14831ed238ad4d8441619d55c1c9
john cheung <1269969739@qq.com>
2018-05-09 23:39:42 +0800

    Update 180512.py

--- Daily/180512.py
+++ Daily/180512.py
@@ -1,19 +1,20 @@
 import time
-from functools import wrap
+from functools import wraps
 
 
 def timer(func):
-    @wrap
-    def wrapper():
+    @wraps(func)
+    def wrapper(*args, **kw):
         start_time = time.time()
-        func()
+        func(*args, **kw)
         stop_time = time.time()
         print("it costs %s run time %s (s)"%(func.__name__, stop_time-start_time))
+        return func(*args, **kw)
     return wrapper
     
     
     
-# 写一个计算函数执行时间的装饰器
+# 写一个计算函数执行时间的装饰器 不失去原始名称和文档字符串的解决方案
 
 # 不能修改被装饰的函数的源代码
 # 不能修改被装饰的函数的调用方式
