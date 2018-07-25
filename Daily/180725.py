ae4855ae5ed5f6fe7c7462d7f7d76be9ecae8c40
john cheung <zhangjohn202@gmail.com>
2018-07-25 16:03:10 +0800

    Create 180725.py

---
+++ Daily/180725.py
@@ -0,0 +1,20 @@
+
+## asyncio + aiohttp
+## async/await是Python提供的异步编程API，而asyncio只是一个利用 async/await API进行异步编程的框架
+
+import aiohttp
+
+
+async def fetch_async(a):
+    async with aiohttp.request('GET', URL.format(a)) as r:
+        data = await r.json()
+    return data['args']['a']
+    
+    
+start = time.time()
+event_loop = asyncio.get_event_loop()
+tasks = [fetch_async(num) for num in NUMBERS]
+results = event_loop.run_until_complete(asyncio.gather(*tasks))
+
+for num, result in zip(NUMBERS, results):
+    print('fetch({}) = {}'.format(num, result))
